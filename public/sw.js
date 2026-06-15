d59c34888e88b4de1230666ecd64fb4c940cb831
bilalelhamri2006-alt <bilalelhamri2006@gmail.com>
2026-06-15 17:27:06 +0100

    fix: remove service worker and clean up caching issues on mobile

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = 'hotel-manager-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logo.png'
-];
-
+// Self-destroying service worker to clear cache and unregister
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request);
-      })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    }).then(() => {
+      return self.registration.unregister();
+    })
   );
 });
